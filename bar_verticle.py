b387b329d251973cfa4815522ef42e6c1db54f5f
gayan <hewagegayanpriyadarshana@gmail.com>
2023-12-07 15:41:10 +0530

    added text inside bar

--- bar_verticle.py
+++ bar_verticle.py
@@ -5,13 +5,15 @@ import pandas as pd
 # Sample dataset based on the provided image
 data = pd.DataFrame({
     "Quarter": ["Q1", "Q2", "Q3", "Q4"],
-    "Positive Comments": [0.20, 0.30, 0.35, 0.40]  # Values as proportions
+    "Positive Comments": [0.20, 0.30, 0.35, 0.40]
+      # Values as proportions
 })
 
 # Creating the bar chart using Plotly
 fig = px.bar(data, x='Quarter', 
              y='Positive Comments', 
-              title='<b><br><span style="font-size: 22px;">Customer Feedback</b><br><span style="font-size: 22px;">Positive Comments</span>',
+              title='<b><br><span style="font-size: 22px;">Customer Feedback</b><br><span style="font-size: 12px;">Positive Comments</span>',
+              text='Positive Comments'
              )
 
 # Updating the layout to show the y-axis tick labels as percentages
@@ -20,7 +22,7 @@ fig.update_layout(yaxis_tickformat='.0%',
                   yaxis_title='')
 
 # Convert the 'Positive Comments' column to percentage format for the bar text
-#fig.update_traces(texttemplate='%{text:.0%}', textposition='outside')
+fig.update_traces(texttemplate='%{text:.0%}', textposition='inside')
 
 # Streamlit command to render the plot
 st.plotly_chart(fig)
