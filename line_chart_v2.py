a3b797dae30313c1ead0db1a2d08338faee5150b
gayan <hewagegayanpriyadarshana@gmail.com>
2023-12-08 21:31:54 +0530

    chenge the side of the

--- line_chart_v2.py
+++ line_chart_v2.py
@@ -1,49 +1,56 @@
 import streamlit as st
-import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
 from datetime import datetime, timedelta
 
 # Generate a sample dataset
 dates = [datetime(2023, 3, 1) + timedelta(days=i) for i in range(31)]
-impressions_this_month = [i * 10 + 500 for i in range(31)]
-clicks_this_month = [i * 5 + 200 for i in range(31)]
-impressions_last_month = [i * 8 + 400 for i in range(31)]
-clicks_last_month = [i * 3 + 150 for i in range(31)]
+data = {
+    'Date': dates + dates + dates + dates,
+    'Value': [i * 10 + 500 for i in range(31)] + [i * 5 + 200 for i in range(31)] +
+             [i * 8 + 400 for i in range(31)] + [i * 3 + 150 for i in range(31)],
+    'Type': ['Impressions'] * 31 + ['Clicks'] * 31 + ['Impressions'] * 31 + ['Clicks'] * 31,
+    'Month': ['This Month'] * 62 + ['Last Month'] * 62
+}
 
-# Create traces for the figure
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=dates, y=impressions_this_month, mode='lines', name='Impressions', line=dict(color='blue')))
-fig.add_trace(go.Scatter(x=dates, y=clicks_this_month, mode='lines', name='Clicks', line=dict(color='purple')))
-fig.add_trace(go.Scatter(x=dates, y=impressions_last_month, mode='lines', name='This month', line=dict(dash='dot', color='blue')))
-fig.add_trace(go.Scatter(x=dates, y=clicks_last_month, mode='lines', name='Last month', line=dict(dash='dot', color='purple')))
+# Convert to DataFrame
+df = pd.DataFrame(data)
 
-# Create a dual-axis layout
+# Create the line chart using Plotly Express
+fig = px.line(df, x='Date', y='Value', color='Type', line_dash='Month',
+              color_discrete_map={'Impressions': 'blue', 'Clicks': 'purple'})
+
+# Update layout for dual y-axes
 fig.update_layout(
     title='All Campaigns',
-    xaxis_title='Date',
+    xaxis_title='',
     yaxis=dict(
-        title='Impressions',
-        side='left',
-        color='blue',
+        title='',
+        
+        side='right',
+        showgrid=True,
     ),
     yaxis2=dict(
         title='Clicks',
-        side='right',
         overlaying='y',
-        color='purple',
+        side='left',
+        showgrid=False,
     ),
+    xaxis=dict(
+        ticks= 'outside',
+        ticklen=6,
+        tickwidth=2,
+        showline=True,
+        linecolor="lightgray"
+    ),
+    legend_title_text='',
     legend=dict(
         orientation='h',
         yanchor='bottom',
-        y=1.02,
+        y=-.3,
         xanchor='right',
         x=1
     )
-)
-
-# Format x-axis tick marks to show Mar 1, Mar 15, Mar 31
-fig.update_xaxes(
-    tickvals=[dates[0], dates[14], dates[-1]],
-    tickformat='%b %d'
 )
 
 # Streamlit command to display the plot
